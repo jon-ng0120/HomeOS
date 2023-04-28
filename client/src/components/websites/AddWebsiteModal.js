f83db25eee3c57a8dee5f644f6a9f719be092a6f
Jon Ng <jonathanng15@gmail.com>
2023-04-27 22:20:49 -0700

    Removed unneeded styling

--- client/src/components/websites/AddWebsiteModal.js
+++ client/src/components/websites/AddWebsiteModal.js
@@ -1,19 +1,57 @@
+import { useState } from 'react';
 import classes from './AddWebsiteModal.module.scss';
 
 const AddWebsite = ({ closeModal }) => {
+  const [website, setWebsite] = useState('');
+  const [url, setUrl] = useState('');
+
+  const formHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const googleId = localStorage.getItem('googleId');
+      const res = await fetch('http://localhost:8080/website/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          googleId,
+          website,
+          url,
+        }),
+      });
+      if (res.status === 204) {
+        setWebsite('');
+        setUrl('');
+      } else {
+        console.log('not 204');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <div className={classes.overlay} onClick={closeModal} />
       <div className={classes.add_website_container}>
         <h2>Add Website</h2>
-        <form>
+        <form onSubmit={formHandler}>
           <div>
             <label>Website Name</label>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => setWebsite(e.target.value)}
+              value={website}
+            />
           </div>
           <div>
             <label>Website URL</label>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => setUrl(e.target.value)}
+              value={url}
+            />
           </div>
           <div className={classes.form_actions}>
             <button
