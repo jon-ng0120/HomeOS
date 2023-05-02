a63072321cdee387a502786f211de47104e6ed2e
Jon Ng <jonathanng15@gmail.com>
2023-05-01 17:00:41 -0700

    Added validation logic for adding websites

---
+++ client/src/utilities/utilities.js
@@ -0,0 +1,37 @@
+export const roundTemperatures = (num) => {
+  return Math.round(num);
+};
+
+export const formatDate = (dateStr) => {
+  const date = new Date(Date.parse(dateStr));
+  return date.toDateString();
+};
+
+export const formatTime = (dateStr) => {
+  const date = new Date(Date.parse(dateStr));
+  return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+};
+
+export const validation = ({ website, url }) => {
+  const errors = {};
+  console.log(website, url);
+
+  if (website.trim().length === 0) {
+    errors.website = true;
+  }
+
+  if (!checkUrl(url)) {
+    errors.url = true;
+  }
+
+  return errors;
+};
+
+const checkUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
