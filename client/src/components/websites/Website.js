8f9f1743521987b66f35e40277e289dbd01f03a8
Jon Ng <jonathanng15@gmail.com>
2023-05-04 00:30:48 -0700

    added onclick handler to open more options menu

--- client/src/components/websites/Website.js
+++ client/src/components/websites/Website.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import classes from './Website.module.scss';
+import WebsiteOptions from './WebsiteOptions';
 
 const Website = ({ website }) => {
+  const [optionsMenu, setOptionsMenu] = useState(false);
+
   return (
     <div className={classes.website}>
-      <span className={`material-icons ${classes.more_options}`}>
+      <span
+        className={`material-icons ${classes.more_options}`}
+        onClick={() => setOptionsMenu(!optionsMenu)}
+      >
         more_vert
       </span>
+      {optionsMenu && <WebsiteOptions />}
       <a href={website.url} target="_blank">
         <img src={website.icon} />
         <p>{website.name}</p>
---
+++ client/src/components/websites/WebsiteOptions.js
@@ -0,0 +1,14 @@
+const WebsiteOptions = () => {
+  return (
+    <div>
+      <p>
+        <span className="material-icons">edit</span>Edit
+      </p>
+      <p>
+        <span className="material-icons">clear</span>Delete
+      </p>
+    </div>
+  );
+};
+
+export default WebsiteOptions;
